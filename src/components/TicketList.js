53639b410a054b922ca7618fd0dee5db4145e6e4
Matt Stroud <lixingke06@gmail.com>
2020-07-06 10:01:19 -0700

    update homework to include redux

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -2,19 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import Ticket from "./Ticket";
 
-function TicketList(props){
+function TicketList(props) {
   return (
     <React.Fragment>
-      <hr/>
-      {props.ticketList.map((ticket) =>
-        <Ticket
-          whenTicketClicked = { props.onTicketSelection }
+      <hr />
+      {Object.values(props.ticketList).map((ticket) => {
+        return <Ticket
+          whenTicketClicked={props.onTicketSelection}
           names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
           id={ticket.id}
-          key={ticket.id}/>
-      )}
+          key={ticket.id} />
+      })}
     </React.Fragment>
   );
 }
